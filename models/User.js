7d245688157be033051ed07e27bb44bb4d29beae
Denise <dmtuominen@gmail.com>
2021-10-13 21:43:15 -0600

    fixed moment in user

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const moment = require('moment');
 
 const UserSchema = new Schema({
     username: {
